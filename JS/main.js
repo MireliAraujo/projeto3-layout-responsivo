c5b40a6d7879266923409834fbe1a928eaf3e3c9
Mireli Araujo <112290519+jonascipriano48@users.noreply.github.com>
2023-08-15 22:33:42 -0300

    Commit inicial

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
+
+menuToggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
